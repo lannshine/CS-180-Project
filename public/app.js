33ce55bcdec1bb676dc37e8662ccf069f8876fd2
Yulin Zhang <32946065+lannshine@users.noreply.github.com>
2019-10-23 03:14:56 -0700

    Update FirebaseUI scripts

--- public/app.js
+++ public/app.js
@@ -1,7 +1,3 @@
 	(function(){
-		const preObject = document.getElementById('object');
-		const dbRefObject = firebase.database().ref().child('object');
-		dbRefObject.on('value', snap => {
-		preObject.innerText = JSON.stringify(snap.val(), null, 3);
-		});
+		
 	}());
