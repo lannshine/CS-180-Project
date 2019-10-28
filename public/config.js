e1a2245f6c4392bc7add14e407a21b0b2b9b42ab
Yulin Zhang <32946065+lannshine@users.noreply.github.com>
2019-10-28 13:18:15 -0700

    Update index.html

---
+++ public/config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAHYEv-liS-ig01tW3YX5-LEUxk3kivmHU",
+    authDomain: "cs-180-project.firebaseapp.com",
+    databaseURL: "https://cs-180-project.firebaseio.com",
+    projectId: "cs-180-project",
+    storageBucket: "cs-180-project.appspot.com",
+    messagingSenderId: "512669686416",
+    appId: "1:512669686416:web:cda495d557dfdc7a919615",
+    measurementId: "G-79NPH0TCD8"
+};
+
+// Google OAuth Client ID, needed to support One-tap sign-up.
+// Set to null if One-tap sign-up is not supported.
+var CLIENT_ID = 'YOUR_OAUTH_CLIENT_ID';
